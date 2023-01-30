81797d5c96de9a5d35538e9b90bed0364be96512
Igor Sharendo <axe_dream@list.ru>
2023-01-30 10:52:39 +0700

    эстетические изменения"

--- views/layouts/main-login.php
+++ views/layouts/main-login.php
@@ -4,8 +4,6 @@
 /* @var $content string */
 
 \hail812\adminlte3\assets\AdminLteAsset::register($this);
-$this->registerCssFile('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700');
-$this->registerCssFile('https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css');
 \hail812\adminlte3\assets\PluginAsset::register($this)->add(['fontawesome', 'icheck-bootstrap']);
 ?>
 <?php $this->beginPage() ?>
@@ -14,7 +12,7 @@ $this->registerCssFile('https://code.ionicframework.com/ionicons/2.0.1/css/ionic
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>CRM: Авторизация</title>
+    <title>ONPN: Авторизация</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
@@ -24,7 +22,7 @@ $this->registerCssFile('https://code.ionicframework.com/ionicons/2.0.1/css/ionic
 <?php  $this->beginBody() ?>
 <div class="login-box">
     <div class="login-logo">
-        <a href="<?=Yii::$app->homeUrl?>"><b>CRM</b></a>
+        <a href="<?=Yii::$app->homeUrl?>"><b>ONPN</b></a>
     </div>
     <!-- /.login-logo -->
 
--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <div class="brand-link">
-        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="CRM Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="ONPN Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">ONPN</span>
     </div>
 
